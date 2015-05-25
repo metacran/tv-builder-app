311897b2ae2052fea67f9a5878bdc799abd97fc6
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-25 10:38:08 -0400

    Rewrite to make it cleaner, stub for tv-pkg updates

--- scheduler.js
+++ scheduler.js
@@ -1,6 +1,12 @@
 var debug = require('debug');
-var build_tv = require('./lib/build_tv');
 var amqp = require('amqplib');
+var got = require('got');
+var cheerio = require('cheerio');
+var async = require('async');
+var build_tv = require('./lib/build_tv');
+var add_tv_links = require('./lib/add_tv_links');
+
+var tv_url = 'http://cran.rstudio.org/web/views/';
 
 function run() {
 
@@ -21,40 +27,37 @@ function run() {
 		var msg_obj = JSON.parse(msg.content.toString());
 		console.log(msg_obj.package + " start.");
 
-		build_tv(msg_obj, function(error, html) {
-		    if (!error) {
-			if (html != "") {
-			    store_html(msg_obj.package, html);
-			}
-			console.log(msg_obj.package + " done.");
-		    } else {
-			console.log(msg_obj.package + ' error.');
+		// Get the doc
+		var tv = msg_obj.package;
+		var url = tv_url + tv + '.ctv';
+		got(url, function (err, data, res) {
+		    if (err || res.statusCode != 200) {
+			console.log(tv + ' error.');
+			ch.ack(msg);
+			return;
 		    }
-		    ch.ack(msg);
-		});
+
+		    var $ = cheerio.load(data);
+
+		    async.parallel(
+			{
+			    'links': function(cb) {
+				add_tv_links(tv, $, function(e, r) { cb(e, r) }) },
+			    'doc': function(cb) {
+				build_tv(tv, $, function(e, r) { cb(e, r) }) }
+			},
+			function(error, results) {
+			    if (error) {
+				console.log(tv + ' error.');
+			    } else {
+				console.log(tv + ' done.');
+			    }
+			    ch.ack(msg)
+			})
+		})
 	    }
 	})
     }).then(null, console.warn);
-}
-
-function store_html(package, html) {
-    var couch_url = process.env.DOCSDB_URL || 'http://127.0.0.1:5984';
-    var nano = require('nano')(couch_url);
-    var db = nano.db.use('task-view');
-    var doc = { 'date': new Date().toISOString(),
-		'html': html };
-
-    db.update = function(obj, key, callback) {
-	var db = this;
-	db.get(key, function (error, existing) {
-	    if(!error) obj._rev = existing._rev;
-	    db.insert(obj, key, callback);
-	});
-    }
-
-    db.update(doc, package, function(error, response) {
-	if (error) { return console.log(error); }
-    });
 }
 
 module.exports = run;
